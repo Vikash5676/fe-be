db86baf4f9f79d9009443b5c3214d43840bbba52
vikash kumar <vikashkumar04081999@gmail.com>
2022-05-28 20:43:43 +0530

    modified src i npm axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value,setValue] = useState('')
+
+
+  const handleChange = (e)=>{
+    setValue(e.target.value)
+  }
+  const handleClick = () =>{
+    axios.post('http://localhost:4010/send',{value:value})
+  }
   return (
     <div className="App">
-      
+      <input type='text' onChange={handleChange}/>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
